d217b743a8090ed48e7e894aad192f14b00f8291
showq <showqonline@gmail.com>
2014-10-29 17:18:01 +0800

    Add more judgment
    database is exists,
    all tables types checking

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,38 +1,84 @@
 #!/bin/bash
 # Program
-#   Mysql数据库备份脚本, 使用Mysqlhotcopy备份myisam类型表
-#   用Mydumper备份innodb类型表
-# Author showq@qq.com
+#  Mysql数据库备份脚本
+#  使用Mysqlhotcopy备份myisam类型表
+#  用Mydumper备份innodb类型表
+# Author Ryanlau<showq#qq.com>
 # History
 #   2014/10/23-1.0
 # Help
+#  读取conf/xx.conf
+
+#
+
+# DEBUG=true
+
+# trap 'echo "before execute line:$LINENO, OPTIONS=${OPTIONS[@]}"' DEBUG
+
+#######################################
+# Initialize variables
+#######################################
+OLD_DIR=${1:-`pwd`}
+EXEC_DIR=${1:-`dirname $0`}
+# your MySQL server's name
+SERVER=`hostname -f`
+# your mysqlhotcopy
+MYHOTCOPY=$(which mysqlhotcopy)
+# your mydumper
+MYDUMPER=$(which mydumper)
+
+#######################################
+# Functions
+#######################################
 function die()
 {
 	echo >&2 "$@"
 	exit 1
 }
+#
 function mysqlCommand(){
-  # echo `mysql -h ${OPTIONS[0]} --user=${OPTIONS[1]} --password=${OPTIONS[2]} -Bse "$1"`;
   echo `mysql -h $1 --user=$2 --password=$3 -Bse "$4"`;
 }
-function commentLine(){
-  echo -e "\033[$1#------ $2 ------ \033[0m"
-}
+#
 function startCommentLine(){
-  echo -e "\033[36m#################### \033[0m"
-  echo -e "\033[36m##$1 \033[0m"
+  echo -e "\033[$1#################### \033[0m"
+  echo -e "\033[$1##$2 \033[0m"
 }
+#
+function commentLine(){
+  echo -e "\033[$1#------ $2 \033[0m"
+}
+#
 function endCommentLine(){
-  echo -e "\033[36m#################### \033[0m"
+  echo -e "\033[$1##$2 \033[0m"
+  echo -e "\033[$1#################### \033[0m"
   echo " "
   echo " "
 }
+#
+function alertMsg(){
+  startCommentLine $1
+  commentLine 31m $2
+  endCommentLine
+}
+#
+function noticeMsg(){
+  startCommentLine $1
+  commentLine 34m $2
+  endCommentLine
+}
+#
+function successMsg(){
+  startCommentLine $1
+  commentLine 32m $2
+  endCommentLine
+}
+
+# Switch execute directory
+cd $EXEC_DIR
+
 CONFIG=${1:-`dirname $0`/mysql-backup.conf}
 [ -f "$CONFIG" ] && . "$CONFIG" || die "Could not load configuration file ${CONFIG}!"
-
-OLD_DIR=${1:-`pwd`}
-EXEC_DIR=${1:-`dirname $0`}
-cd $EXEC_DIR
 
 # check of the backup directory exists
 # if not, create it
@@ -57,23 +103,38 @@ do
   OPTIONS[5]=${DBMAIL:-${BACKUP_MAIL}}
   OPTIONS[6]=${DBEMAILS:-${BACKUP_EMAILS}}
 
-  #@TODO: Add debug options echo ${OPTIONS[@]}
-
 	if [ -z "${OPTIONS[4]}" ]; then
-    startCommentLine "Creating list of all your databases..."
     DBS=`mysqlCommand ${OPTIONS[0]} ${OPTIONS[1]} ${OPTIONS[2]} "Show databases;"`
-    commentLine 32m "Please modify config file, Usage "
-    commentLine 32m "${DBS}"
-    endCommentLine
-    exit 0
+    TITLE="Creating list of all your databases... \n\n"
+    BODY="Please modify config file, Usage "
+    BODY=$BODY$DBS
+    alertMsg $TITLE $BODY
 	fi
 
-	#Check required parameter
-	# @TODO
-	# 1.数据库存在，有权限备份。
-	# 2.数据库中表类型与配置相符。
+  # Fetch all tables
+  DBC=`mysqlCommand ${OPTIONS[0]} ${OPTIONS[1]} ${OPTIONS[2]} "use ${OPTIONS[4]}; show tables;"`
 
-  #@TODO 忽略表
+  if [ -z "${DBC}" ]; then
+    alertMsg "Please check dbname" "${OPTIONS[4]} is non-exist"
+  fi
+
+  CHECKTYPE=true
+  for tbl in ${DBC}
+  do
+    myc="use ${OPTIONS[4]}; SHOW TABLE STATUS FROM ${OPTIONS[4]} WHERE name='$tbl';"
+    DBD=`mysqlCommand ${OPTIONS[0]} ${OPTIONS[1]} ${OPTIONS[2]} "$myc"`
+    if [[ -z "$DBD" ]]; then
+      CHECKTYPE=false
+      break;
+    fi
+  done
+
+  if [[ !$CHECKTYPE ]]; then
+    alertMsg "Type is error" "Body"
+  fi
+
+
+  # @TODO 忽略表
 	# filter out the tables to backup
 	# if [ -n "${DBTABLES}" ]; then
 	#   if  [ ${DBTABLESMATCH} = "exclude" ]; then
@@ -86,7 +147,8 @@ do
 	#   fi
 	# fi
 
-  # echo -n "Backing up database ${OPTIONS[4]}..."
+  # 针对DRUPAL优化。不存储缓存表数据
+
   startCommentLine "Backing up MySQL database ${OPTIONS[4]} on ${OPTIONS[0]}..."
   DB_BACKDIR=${BACKDIR}/${OPTIONS[4]}
   DB_DATE_BACKDIR=${DB_BACKDIR}/${DATE}
@@ -106,6 +168,8 @@ do
     # 压缩比-6
   fi
   # mysqldump
+  # mysqldump -h ${DBHOST[$KEY]} --user=${DBUSER[$KEY]} --password=${DBPASS[$KEY]} ${DBOPTIONS[$KEY]} $database $TABLES > \
+  # $BACKDIR/$SERVER-$database-$DATE-mysqlbackup.sql
 
   # Check backup
   BAK_FILENAME=${OPTIONS[4]}-${DATE//\:/-}.tar.gz
@@ -118,13 +182,15 @@ do
     cd -
     sudo rm -rf $DB_DATE_BACKDIR
 
-    #@TODO: 增加压缩发送邮箱功能
+    # if you have the mail program 'mutt' installed on
+    # your server, this script will have mutt attach the backup
+    # and send it to the email addresses in $EMAILS
     if  [ ${OPTIONS[5]} = "y" ]; then
       BODY="Your backup is ready! \n\n"
       BODY=$BODY`cd $DB_BACKDIR; md5sum ${BAK_FILE};`
       ATTACH=` echo -n "-a ${BAK_FILE} "; `
 
-      echo -e "$BODY" | mutt -s "$SUBJECT" $ATTACH -- ${OPTIONS[6]}
+      # echo -e "$BODY" | mutt -s "$SUBJECT" $ATTACH -- ${OPTIONS[6]}
       #if [[ $? -ne 0 ]]; then
       #  echo -e "ERROR:  Your backup could not be emailed to you! \n";
       #else
@@ -133,12 +199,7 @@ do
     fi
   fi
 
-  # mysqldump -h ${DBHOST[$KEY]} --user=${DBUSER[$KEY]} --password=${DBPASS[$KEY]} ${DBOPTIONS[$KEY]} $database $TABLES > \
-  # $BACKDIR/$SERVER-$database-$DATE-mysqlbackup.sql
 
-	# if you have the mail program 'mutt' installed on
-	# your server, this script will have mutt attach the backup
-	# and send it to the email addresses in $EMAILS
 
 
 	# if  [ $DELETE = "y" ]; then
@@ -156,5 +217,4 @@ do
   endCommentLine "The database ${OPTIONS[4]} is backed up!"
 	unset OPTIONS
 done
-
-echo $OLD_DIR
+cd $OLD_DIR
