2af77fcd585f85247abd0997cceea47a3fadf88a
showq <showqonline@gmail.com>
2015-01-05 16:30:36 +0800

    Add more function

--- cron_unidump.sh
+++ cron_unidump.sh
@@ -10,173 +10,256 @@
 # Help
 #  此脚本会读取$HOME/mysql-backup下的xx.conf
 #
-
 #######################################
-# for debug 
+# for debug
 # trap 'echo "before execute line:$LINENO, OPTIONS=${OPTIONS[@]}"' DEBUG
 #######################################
 
 #######################################
 # Functions
 #######################################
-function die()
-{
-	echo >&2 "$@"
-	exit 1
+#
+function die(){
+  echo >&2 "$@"
+  exit 1
 }
 #
 function mysqlCommand(){
   echo `mysql -h $1 --user=$2 --password=$3 -Bse "$4"`;
 }
+function getColor(){
+  case $1 in
+    'alert')
+      COLOR="31m"
+      ;;
+    'notice')
+      COLOR="34m"
+      ;;
+    'success')
+      COLOR="32m"
+      ;;
+  esac
+}
 #
 function startCommentLine(){
-  echo -e "\033[$1#################### \033[0m"
-  echo -e "\033[$1##$2 \033[0m"
+  getColor $1
+  echo -e "\033[$COLOR#################### \033[0m"
+  echo -e "\033[$COLOR# $2 \033[0m"
 }
 #
 function commentLine(){
-  echo -e "\033[$1#------ $2 \033[0m"
+  getColor $1
+  echo -e "\033[$COLOR# $2 \033[0m"
 }
 #
 function endCommentLine(){
+  getColor $1
   # echo -e "\033[$1##$2 \033[0m"
-  echo -e "\033[$1#################### \033[0m"
+  echo -e "\033[$COLOR#################### \033[0m"
   echo " "
-  echo " "
+}
+#
+function commonMsg(){
+  startCommentLine "$3" "$1"
+  if [[ -n $2 ]]; then
+    commentLine "$3" "$2"
+  fi
+  endCommentLine "$3"
 }
 #
 function alertMsg(){
-  startCommentLine 31m $1
-  commentLine 31m $2
-  endCommentLine 31m
+  commonMsg "$1" "$2" 'alert'
 }
 #
 function noticeMsg(){
-  startCommentLine 34m $1
-  commentLine 34m $2
-  endCommentLine 34m
+  commonMsg "$1" "$2" 'notice'
 }
 #
 function successMsg(){
-  startCommentLine 32m $1
-  commentLine 32m $2
-  endCommentLine 32m
+  commonMsg "$1" "$2" 'success'
 }
-
+# check of the directory exists
+# if not, create it
+function createDirectory(){
+  if [ ! -d $1 ]; then
+    mkdir -p $1
+    if [ -d $1 ]; then
+      successMsg "Successfully create the directory: $1"
+    fi
+  fi
+}
 # $1 $fileBaseDir
 # $2 $dbBackDir
 # $3 $logBaseDir
 function necessaryDirectory(){
-  # check of the backup directory exists
-  # if not, create it
-  if [ ! -d $1 ]; then
-    echo -n "Creating $1..."
-    mkdir -p $1
-    echo "done!"
-  fi
 
-  #dbBaseDir
-  if [ ! -d $2 ]; then
-    echo -n "Creating $2..."
-    mkdir -p $2
-    echo "done!"
-  fi
-
-  #logBaseDir
-  if [ ! -d $3 ]; then
-    echo -n "Creating $3..."
-    mkdir -p $3
-    echo "done!"
-  fi
+  createDirectory $1
+  createDirectory $2
+  createDirectory $3
 
   if [ ! -d $1 ] || [ ! -d $2 ] || [ ! -d $3 ]; then
-    TITLE="Miss required directory"
-    BODY="Miss required directory, Please manually create"
-    alertMsg $TITLE $BODY
+    alertMsg "Miss required directory" "Miss required directory, Please manually create"
   fi
 }
+#
+function unidump_initEnv(){
+  # install tip
+  CONFIG="$HOME/.cron_unidump.conf"
+  [ -f "$CONFIG" ] && . "$CONFIG" || die "Could not load configuration file ${CONFIG}! You can run ./cron_unidump.sh install"
+  configDir="$HOME/.cron_unidump.d"
+}
+# ------------------
+# Check mysql config
+# ------------------
+function unidump_backup_db_check(){
+  # Invoke default settings
+  DBHOST=${DBHOST:-${BACKUP_HOST}}
+  DBUSER=${DBUSER:-${BACKUP_USER}}
+  DBPASS=${DBPASS:-${BACKUP_PASS}}
+  DBENGINE=${DBENGINE:-"myisam"}
+  DBNAME=${DBNAME}
+  DBMAIL=${DBMAIL:-${BACKUP_MAIL}}
+  DBMAILTO=${DBMAILTO:-${BACKUP_MAILTO}}
+  DBOPTIONS=${DBOPTIONS:-""}
 
+  DBSQLDUMP_TABLES=${DBSQLDUMP_TABLES:-""}
 
-#######################################
-# install shell
-if [ $0 = 'install' ]; then
-  # @TODO Move global configuration file to /etc/
-  echo 'Installed '
-fi
+  CHECK_RESULT=true
 
-#######################################
-# Initialize variables
-#######################################
-#shell文件存放目录
-initDir=${1:-`pwd`}
-#执行目录
-execDir=${1:-`dirname $0`}
+  # 没有设置DBNAME
+  if [ -z "$DBNAME" ]; then
+    DBS=`mysqlCommand $DBHOST $DBUSER $DBPASS "Show databases;"`
+    TITLE="Creating list of all your databases... \n\n"
+    BODY="Please modify config file, Usage "$DBS
+    alertMsg $TITLE $BODY
+    CHECK_RESULT=false
+  fi
 
-# your mysql server's name
-server=`hostname -f`
-# your mysqlhotcopy
-myHotcopy=$(which mysqlhotcopy)
-# your mydumper
-myDumper=$(which mydumper)
-# your mysqldump
-mysqlDump=$(which mysqldump)
+  # Fetch all tables from databases
+  DB_TABLE_NAMES=`mysqlCommand $DBHOST $DBUSER $DBPASS "use $DBNAME; show tables;"`
 
-# user config file directory for each sites
-configDir=$HOME/.cron_unidump.d
-#configDir
-if [ ! -d $configDir ]; then
-  echo -n "Creating $configDir..."
-  mkdir -p $configDir
-  echo "done!"
-fi
+  # If DB_TABLE_NAMES is empty, DBNAME no exists
+  if [ -z "$DB_TABLE_NAMES" ]; then
+    alertMsg "Please check dbname name in $confFile $DBNAME is non-exist"
+    CHECK_RESULT=false
+  fi
 
-########################################
-# Basic backup file store directory
-########################################
-# file
-G_fileBaseDir=/var/backup/files
-# database
-G_dbBaseDir=/var/backup/mysql
-# log
-G_logBaseDir=/var/log/backup
+  # 检查库中，所有的表引擎与配置文件设置一致
+  CHECKTYPE=true
+  DBENGINE_UPPERCASE=$(echo $DBENGINE | tr '[a-z]' '[A-Z]');
 
-# date format that is appended to filename
-dateStr=$(date -u +'%F-%T')
-dateStrSuffix=${dateStr//\:/-}
+  for tbl in $DB_TABLE_NAMES ; do
+    #myc="use ${DBNAME}; SHOW TABLE STATUS FROM ${DBNAME} WHERE name='$tbl';"
+    myCommand="use information_schema;"
+    myCommand=$myCommand"select engine from information_schema.tables where table_schema='$DBNAME' AND table_name='$tbl';"
+    tblENGINE=`mysqlCommand $DBHOST $DBUSER $DBPASS "$myCommand"`
 
-fileOwn='vagrant:vagrant'
+    tblENGINE_UPPERCASE=$(echo $tblENGINE | tr '[a-z]' '[A-Z]');
 
-#######################################
-# Shell program
-#######################################
-# @TODO Add global configuration
+    if [[ "$tblENGINE_UPPERCASE" != "$DBENGINE_UPPERCASE" ]]; then
+      CHECKTYPE=false
+      break;
+    fi
+  done
 
-# Read basic configuration
-CONFIG=${1:-`dirname $0`/cron_unidump.conf}
-[ -f "$CONFIG" ] && . "$CONFIG" || die "Could not load configuration file ${CONFIG}!"
+  if [ ! $CHECKTYPE ]; then
+    TITLE="table engine have differences on database "$DBNAME
+    BODY="You can change the table engine to "$DBENGINE_UPPERCASE
+    alertMsg $TITLE $BODY
+    CHECK_RESULT=false
+  fi
 
+  if [[ ! $CHECK_RESULT ]]; then
+    exit 1
+  fi
+}
+#
+#
+function unidump_backup_db(){
+  # Start backup database
+  commentLine 'notice' "------------------ DB begin"
+  commentLine 'notice' "Backing up MySQL database $DBNAME on $DBHOST..."
+  dbBackDir=$dbBaseDir/$DBNAME
+  dbBackDir_date=$dbBackDir/$dateStr
+  if [ ! -d $dbBackDir_date ]; then
+    commentLine 'notice' "Creating $dbBackDir_date for $DBNAME..."
+    mkdir -p $dbBackDir_date
+  fi
 
-for f in $configDir/*.conf ; do
-	#Read .conf file
-	INFO=`cat $f | grep -v ^$ | sed -n "s/\s\+//;/^#/d;p" ` && eval "$INFO"
-  # echo $INFO
+  test $DBHOST == "localhost" && server=`hostname -f` || server=$DBHOST
 
-  NAME=${f#*/.cron_unidump.d/}
-  NAME=${NAME%%.conf}
+  case $DBENGINE in
+    "myisam")
+      # mysqlhotcopy
+      dumpCommand="sudo $myHotcopy -u $DBUSER -p $DBPASS --addtodest $DBNAME$DBOPTIONS $dbBackDir_date";;
+    "mydumper")
+      # mydumper
+      dumpCommand="$myDumper -o $dbBackDir_date -r 10000 -c -e -u $DBUSER -p $DBPASS -B $DBNAME $DBOPTIONS";;
+    "mysqldump")
+      # mysqldump
+      dumpCommand="$mysqlDump -h $DBHOST --user=$DBUSER --password=$DBPASS --add-drop-table $DBOPTIONS $DBNAME $DBSQLDUMP_TABLES -r$dbBackDir_date/backup.sql";;
+  esac
 
-  startCommentLine 32m "Start processing based on $f"
+  commentLine 33m "Command: $dumpCommand"
+  commandMsg=$($dumpCommand)
+  commentLine 32m "$commandMsg"
 
-  fileBaseDir=${fileDir:-$G_fileBaseDir}
-  dbBaseDir=${dbDir:-$G_dbBaseDir}
-  logBaseDir=${logDir:-$G_logBaseDir}
+  # Check backup
+  BAK_FILENAME=$DBNAME-$dateStrSuffix.tar.gz
+  BAK_FILEPATH=$dbBackDir/$BAK_FILENAME
 
-  necessaryDirectory $fileBaseDir $dbBaseDir $logBaseDir
+  # Mail 发送
+  # 是否存在有效备份
+  if [ -r $dbBackDir_date ] && [ ! -z "$(ls $dbBackDir_date)" ]; then
+    tar -czPf $BAK_FILEPATH $dbBackDir_date
+    sudo rm -rf $dbBackDir_date
 
+    # if you have the mail program 'mutt' installed on
+    # your server, this script will have mutt attach the backup
+    # and send it to the email addresses in $EMAILS
+    if  [ $DBMAIL = "y" ]; then
+      BODY="Your backup is ready! \n\n"
+      BODY=$BODY`cd $dbBackDir; md5sum $BAK_FILEPATH;`
+      ATTACH=` echo -n "-a $BAK_FILEPATH "; `
+
+      # echo -e "$BODY" | mutt -s "$SUBJECT" $ATTACH -- $DBMAILTO
+      #if [[ $? -ne 0 ]]; then
+      #  echo -e "ERROR:  Your backup could not be emailed to you! \n";
+      #else
+      #  echo -e "Your backup has been emailed to you! \n"
+      #fi
+    fi
+  fi
+
+  # @TODO 默认保存1个月中，30个数据备份。
+  # if  [ $DBDELETE = "y" ]; then
+  #  OLDDBS=`cd $BACKDIR; find . -name "*-mysqlbackup.sql.gz" -mtime +$DAYS`
+  #  REMOVE=`for file in $OLDDBS; do echo -n -e "delete ${file}\n"; done`
+
+  #  cd $BACKDIR; for file in $OLDDBS; do rm -v ${file}; done
+  #  if  [ $DAYS = "1" ]; then
+  #    echo "Yesterday's backup has been deleted."
+  #  else
+  #    echo "The backups from $DAYS days ago and earlier have been deleted."
+  #  fi
+  # fi
+
+  commentLine 'notice' "------------------ DB complete"
+  commentLine 'success' "The database $DBNAME is backed up!"
+  endCommentLine 'success'
+
+  # @TODO record backup log
+
+  # unset variables
+  unset DBHOST DBUSER DBPASS
+  unset DBENGINE DBNAME DBMAIL
+  unset DBMAILTO DBSQLDUMP_TABLES
+}
+function unidump_backup_file(){
   # ------------------
   # Backup files
   # ------------------
   # @TODO 增加清除条件
-
   if [ $SOURCE -a -d $SOURCE ]; then
     commentLine 32m "------------------ File backup begin"
     TARGET=$fileBaseDir/$NAME-$dateStrSuffix.tar.bz2
@@ -232,141 +315,143 @@ for f in $configDir/*.conf ; do
 
     cp $logFile $fileBaseDir/
     chown $fileOwn $fileBaseDir/$logFileName
-    echo "----------------------------------------------------" >> $logFile
+    echo "----------------------------------------------------\n" >> $logFile
     commentLine 32m "------------------ File backup complete"
   fi
-
-  # ------------------
-  # Mysql 
-  # ------------------
-	#Invoke default settings
-	DBHOST=${DBHOST:-${BACKUP_HOST}}
-	DBUSER=${DBUSER:-${BACKUP_USER}}
-	DBPASS=${DBPASS:-${BACKUP_PASS}}
-	DBENGINE=${DBENGINE:-"myisam"}
-	DBNAME=${DBNAME}
-  DBMAIL=${DBMAIL:-${BACKUP_MAIL}}
-  DBMAILTO=${DBMAILTO:-${BACKUP_MAILTO}}
-  DBOPTIONS=${DBOPTIONS:-""}
-
-  DBSQLDUMP_TABLES=${DBSQLDUMP_TABLES:-""}
-
-  # 没有设置DBNAME
-	if [ -z "$DBNAME" ]; then
-    DBS=`mysqlCommand $DBHOST $DBUSER $DBPASS "Show databases;"`
-    TITLE="Creating list of all your databases... \n\n"
-    BODY="Please modify config file, Usage "
-    BODY=$BODY$DBS
-    alertMsg $TITLE $BODY
-	fi
-
-  # Fetch all tables from databases
-  DB_TABLE_NAMES=`mysqlCommand $DBHOST $DBUSER $DBPASS "use $DBNAME; show tables;"`
-  
-  # If DB_TABLE_NAMES is empty, DBNAME no exists
-  if [ -z "$DB_TABLE_NAMES" ]; then
-    alertMsg "Please check dbname name in "$f "$DBNAME is non-exist"
-  fi
-
-  # 检查库中，所有的表引擎与配置文件设置一致
-  CHECKTYPE=true
-  DBENGINE_UPPERCASE=$(echo $DBENGINE | tr '[a-z]' '[A-Z]');
-
-  for tbl in $DB_TABLE_NAMES ; do
-    #myc="use ${DBNAME}; SHOW TABLE STATUS FROM ${DBNAME} WHERE name='$tbl';"
-    myCommand="use information_schema;"
-    myCommand=$myCommand"select engine from information_schema.tables where table_schema='$DBNAME' AND table_name='$tbl';"
-    tblENGINE=`mysqlCommand $DBHOST $DBUSER $DBPASS "$myCommand"`
-
-    tblENGINE_UPPERCASE=$(echo $tblENGINE | tr '[a-z]' '[A-Z]');
-
-    if [[ "$tblENGINE_UPPERCASE" != "$DBENGINE_UPPERCASE" ]]; then
-      CHECKTYPE=false
-      break;
-    fi
-  done
-
-  if [ ! $CHECKTYPE ]; then
-    TITLE="table engine have differences on database "$DBNAME
-    BODY="You can change the table engine to "$DBENGINE_UPPERCASE
-    alertMsg $TITLE $BODY
-  fi
-
-  # Start backup
-  commentLine 32m "------------------ DB begin"
-  commentLine 32m "Backing up MySQL database $DBNAME on $DBHOST..."
-  dbBackDir=$dbBaseDir/$DBNAME
-  dbBackDir_date=$dbBackDir/$dateStr
-  if [ ! -d $dbBackDir_date ]; then
-    commentLine 32m "Creating $dbBackDir_date for $DBNAME..."
-    mkdir -p $dbBackDir_date
-  fi
-
-  test $DBHOST == "localhost" && server=`hostname -f` || server=$DBHOST
-
-  case $DBENGINE in
-    "myisam")
-      # mysqlhotcopy
-      C="sudo $myHotcopy -u $DBUSER -p $DBPASS --addtodest $DBNAME$DBOPTIONS $dbBackDir_date";;
-    "mydumper")
-      # mydumper
-      C="$myDumper -o $dbBackDir_date -r 10000 -c -e -u $DBUSER -p $DBPASS -B $DBNAME $DBOPTIONS";;
-    "mysqldump")
-      # mysqldump
-      C="$mysqlDump -h $DBHOST --user=$DBUSER --password=$DBPASS --add-drop-table $DBOPTIONS $DBNAME $DBSQLDUMP_TABLES -r$dbBackDir_date/backup.sql";;
-  esac
-  commentLine 33m "Command: $C"
-  commandMsg=$($C)
-  commentLine 32m "$commandMsg"
-
-  # Check backup
-  BAK_FILENAME=$DBNAME-$dateStrSuffix.tar.gz
-  BAK_FILEPATH=$dbBackDir/$BAK_FILENAME
-
-  # 是否存在有效备份
-  if [ -r $dbBackDir_date ] && [ ! -z "$(ls $dbBackDir_date)" ]; then
-    tar -czPf $BAK_FILEPATH $dbBackDir_date
-    sudo rm -rf $dbBackDir_date
-
-    # if you have the mail program 'mutt' installed on
-    # your server, this script will have mutt attach the backup
-    # and send it to the email addresses in $EMAILS
-    if  [ $DBMAIL = "y" ]; then
-      BODY="Your backup is ready! \n\n"
-      BODY=$BODY`cd $dbBackDir; md5sum $BAK_FILEPATH;`
-      ATTACH=` echo -n "-a $BAK_FILEPATH "; `
-
-      # echo -e "$BODY" | mutt -s "$SUBJECT" $ATTACH -- $DBMAILTO
-      #if [[ $? -ne 0 ]]; then
-      #  echo -e "ERROR:  Your backup could not be emailed to you! \n";
-      #else
-      #  echo -e "Your backup has been emailed to you! \n"
-      #fi
-    fi
-  fi
-
-  # @TODO 默认保存1个月中，30个数据备份。
-	# if  [ $DBDELETE = "y" ]; then
-	#  OLDDBS=`cd $BACKDIR; find . -name "*-mysqlbackup.sql.gz" -mtime +$DAYS`
-	#  REMOVE=`for file in $OLDDBS; do echo -n -e "delete ${file}\n"; done` 
-
-	#  cd $BACKDIR; for file in $OLDDBS; do rm -v ${file}; done
-	#  if  [ $DAYS = "1" ]; then
-	#    echo "Yesterday's backup has been deleted."
-	#  else
-	#    echo "The backups from $DAYS days ago and earlier have been deleted."
-	#  fi
-	# fi
-
-  commentLine 32m "------------------ DB complete"
-  endCommentLine 32m "The database $DBNAME is backed up!"
-
   # unset variables
-  # files
   unset NAME SOURCE TARGET EXCLUDE
+}
+#
+function unidump_backup(){
+  unidump_initEnv
 
-  # mysql
-	unset DBHOST DBUSER DBPASS
-  unset DBENGINE DBNAME DBMAIL
-  unset DBMAILTO DBSQLDUMP_TABLES
-done
+  confFile="$configDir/$2.conf"
+  INFO=`cat $confFile | grep -v ^$ | sed -n "s/\s\+//;/^#/d;p" ` && eval "$INFO"
+  NAME=${confFile#*/.cron_unidump.d/}
+  NAME=${NAME%%.conf}
+
+  startCommentLine 'notice' "Start processing based on $confFile"
+
+  fileBaseDir=${fileDir:-$DEFAULT_fileBaseDir}
+  dbBaseDir=${dbDir:-$DEFAULT_dbBaseDir}
+  logBaseDir=${logDir:-$DEFAULT_logBaseDir}
+
+  necessaryDirectory $fileBaseDir $dbBaseDir $logBaseDir
+
+  # type
+  case $1 in
+    'db')
+      unidump_backup_db $2
+      ;;
+    'file')
+      unidump_backup_file $2
+      ;;
+    'all')
+      unidump_backup_db_check $2
+      unidump_backup_db $2
+      unidump_backup_file $2
+      ;;
+    *)
+      alertMsg "" "Only accept db, file, all"
+      ;;
+  esac
+  endCommentLine 'notice'
+}
+
+# @TODO
+# $1 type
+#   db, file, all
+# $2 name
+#   name
+# $3 date
+#   latest or date
+function unidump_restore(){
+  unidump_initEnv
+
+  alertMsg 'Restore' 'Restore $1 at $2'
+}
+
+#######################################
+# Variables
+#######################################
+# shell文件存放的根目录
+initDir=${initDir:-`pwd`}
+
+# 执行目录
+# execDir=$(dirname $0)
+
+# your mysql server's name
+server=`hostname -f`
+
+# your mysqlhotcopy
+myHotcopy=$(which mysqlhotcopy)
+# your mydumper
+myDumper=$(which mydumper)
+# your mysqldump
+mysqlDump=$(which mysqldump)
+
+########################################
+# Basic backup file store directory
+########################################
+# file
+DEFAULT_fileBaseDir=/var/backup/files
+# database
+DEFAULT_dbBaseDir=/var/backup/mysql
+# log
+DEFAULT_logBaseDir=/var/log/backup
+
+# date format that is appended to filename
+dateStr=$(date -u +'%F-%T')
+dateStrSuffix=${dateStr//\:/-}
+
+fileOwn='vagrant:vagrant'
+
+#######################################
+# Shell Body
+#######################################
+# install this shell scripts
+# example:
+# ./cron_unidump.sh install
+# ./cron_unidump.sh backup $name
+#   backup $configName
+# ./cron_unidump.sh restore $name $date
+#
+# @TODO
+# ./cron_unidump.sh list $name
+
+case $1 in
+  'install')
+    # Copy global config, set custom directory
+    cp $initDir/cron_unidump.conf $HOME/.cron_unidump.conf
+    createDirectory $HOME/.cron_unidump.d
+    cp $initDir/example.eg $HOME/.cron_unidump.d/example.eg
+
+    successMsg 'Success' 'Successfully installed'
+    ;;
+  'uninstall')
+    rm -r $HOME/.cron_unidump.d
+    rm $HOME/.cron_unidump.conf
+    successMsg 'Uninstalled' 'Successfully uninstalled'
+    ;;
+  'create')
+    cp $HOME/.cron_unidump.d/example.eg $HOME/.cron_unidump.d/$2.conf
+
+    successMsg 'Success' 'Successfully created $2'
+    ;;
+  'backup')
+    # db, file, all
+    unidump_backup $2 $3
+
+    ;;
+  'restore')
+    # Required args
+    # @TODO 只恢复数据库
+    # ./cron_unidump.sh db [name] [date]
+    unidump_restore $2 $3
+
+    ;;
+  'list')
+    # --all
+    echo 'List all $2'
+    ;;
+esac
